373b70819f0832e6f22591ded0d7ce845d088b65
Huong Trinh <trtrhuong@gmail.com>
2019-04-11 09:08:37 +0700

    add IssueState

--- src/App/Components/IssueItem/IssueItem.js
+++ src/App/Components/IssueItem/IssueItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {IssueTitle} from './IssueTitle'
 import {IssueContent} from './IssueContent'
-import { IssueOwner } from './IssueOwner';
-
+import { IssueOwner } from './IssueOwner'
+import { IssueState } from './IssueState'
 
 export const IssueItem = (props) => (
 <div className="issueItem">
+<IssueState state={props.issue.state} />
 <IssueOwner avatar_url={props.issue.user.avatar_url} login={props.issue.user.login}/>
 <IssueTitle id={props.issue.id} title={props.issue.title}/>
 <IssueContent body={props.issue.body} />
---
+++ src/App/Components/IssueItem/IssueState.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const IssueState = (props) => (
+    <div className="issueState">
+        <span className="badge badge-primary">{props.state}</span>
+    </div>
+) 
