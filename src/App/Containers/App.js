9186238252cc577da43795351d7670b9bb2779f8
Huong Trinh <trtrhuong@gmail.com>
2019-04-14 00:52:23 +0700

    add sever.js and auth request in app.js

--- src/App/Containers/App.js
+++ src/App/Containers/App.js
@@ -3,12 +3,49 @@ import NavBar from '../Components/Navbar/Navbar'
 import Main from '../Components/Main/Main'
 
 
+const clientId = process.env.REACT_APP_CLIENT_ID;
 
-const App = () => (
-        <div className="App">
-        <NavBar />
-        <Main />
-        </div>
-    )
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        const existingToken = sessionStorage.getItem('token');
+        const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+      
+        if (!accessToken && !existingToken) {
+          window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+        }
+      
+        if (accessToken) {
+          console.log(`New accessToken: ${accessToken}`);
+      
+          sessionStorage.setItem("token", accessToken);
+          this.state = {
+              token: accessToken
+          }
+        }
+      
+        if (existingToken) {
+          this.state = {
+            token: existingToken
+          };
+        }    
+      }
+
+
+      render(){
+          return(
+            <div className="App">
+            <NavBar />
+            <Main />
+            </div>
+          )
+      }
+       
+}
+
+
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
 import App from './App/Containers/App'
 import * as serviceWorker from './serviceWorker';
 
